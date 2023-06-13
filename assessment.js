b66ec7c2d210fdd1cf5e460ac71947cf08100afd
yui2205333 <135851106+yui2205333@users.noreply.github.com>
2023-06-13 19:12:55 +0900

    Add files via upload

---
+++ assessment.js
@@ -0,0 +1,114 @@
+'use strict';
+const userNameInput=document.getElementById('user-name');
+const assessmentButton=document.getElementById('assessment');
+const resultDivided=document.getElementById('result-area');
+const tweetDivided=document.getElementById('tweet-area');
+
+assessmentButton.onclick=()=>{//function()と同義→アロー関数
+  const userName=userNameInput.value;
+  if(userName.length===0){
+    //名前が空のときは処理を終了する
+    return; //関数の処理の中で、 return; は、戻り値なしにそこで処理を終了するという意味です。
+  }
+
+//診断結果表示エリアの作成
+resultDivided.innerText='';//診断結果表示エリアの内側の文字列を空文字列に変更→診断結果表示エリアの子どもの要素を全削除
+
+//headerDividedの作成
+const headerDivided=document.createElement('div');
+headerDivided.setAttribute('class','card-header');
+headerDivided.innerText='診断結果';
+
+//bodyDividedの作成
+const bodyDivided=document.createElement('div');
+bodyDivided.setAttribute('class','card-body');
+
+const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
+  const result = assessment(userName);
+  paragraph.innerText = result;
+  bodyDivided.appendChild(paragraph);
+
+  // resultDivided に Bootstrap のスタイルを適用する
+  resultDivided.setAttribute('class', 'card');
+  resultDivided.setAttribute('style', 'max-width: 700px;')
+
+  // headerDivided と bodyDivided を resultDivided に差し込む
+  resultDivided.appendChild(headerDivided);
+  resultDivided.appendChild(bodyDivided);
+
+//ツイートエリアの作成
+tweetDivided.innerText='';
+const anchor=document.createElement('a');//←顔みたいで可愛い
+const hrefValue=
+  'http://twitter.com/intent/tweet?button_hashtag='+
+  encodeURIComponent('あなたのいいところ')+
+  '&ref_src=twsrc%5Etfw';
+anchor.setAttribute('href',hrefValue);
+anchor.setAttribute('class','twitter-hashtag-button');
+anchor.setAttribute('data-text',result);
+anchor.innerText='Tweet #あなたのいいところ';
+tweetDivided.appendChild(anchor);
+
+//widgets.jsの設定
+const script=document.createElement('script');
+script.setAttribute('src','http://platform.twitter.com/widgets.js');
+tweetDivided.appendChild(script);
+
+userNameInput.onkeydown=event=>{
+  if(event.key==='Enter'){
+    assessmentButton.onclick();
+  }
+}
+};
+
+
+const answers=[//const→再代入出来ない変数＝定数
+'{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+'{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+'{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+'{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+'{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+'{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+'{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+'{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+'{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+'{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+'{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+'{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+'{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+'{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+'{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+'{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
+];
+
+/**
+ * 名前の文字列を渡すと診断結果を返す関数
+ * @param {string} userName ユーザの名前
+ * @return {string} 診断結果
+ */
+function assessment(userName) {
+  // 全文字のコード番号を取得してそれを足し合わせる
+  let sumOfCharCode = 0;
+  for (let i = 0; i < userName.length; i++) {
+    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+  }
+
+  // 文字のコード番号の合計を回答の数で割って添字の数値を求める
+  const index = sumOfCharCode % answers.length;
+  let result = answers[index];
+
+  result=result.replaceAll('{userName}',userName);
+  return result;
+}
+//テストコード
+console.assert(
+  assessment('太郎')===
+  '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+  '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+);
+
+console.assert(
+  assessment('太郎')===assessment('太郎'),
+  '入力が同じなら同じ診断結果を出力する処理が正しくありません。'
+);
